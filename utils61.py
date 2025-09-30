5704b0664b8c2872838ee7e4ad605393d53a73aa
Dan2489 <dan.smith1@justice.gov.uk>
2025-09-30 13:55:00 +0100

    Update utils61.py

--- utils61.py
+++ utils61.py
@@ -62,15 +62,51 @@ def inject_govuk_css() -> None:
     )
 
 # ----------------------------
-# Prison Regions
+# Prison Regions (full mapping)
 # ----------------------------
 PRISON_TO_REGION = {
-    "Brixton": "Inner London",
-    "Liverpool": "National",
-    "Manchester": "National",
-    "Pentonville": "Inner London",
-    "Wandsworth": "Inner London",
-    # ... add back the rest of your full mapping here ...
+    "Altcourse": "National", "Ashfield": "National", "Askham Grange": "National",
+    "Aylesbury": "National", "Bedford": "National", "Belmarsh": "Inner London",
+    "Berwyn": "National", "Birmingham": "National", "Brinsford": "National",
+    "Bristol": "National", "Brixton": "Inner London", "Bronzefield": "Outer London",
+    "Buckley Hall": "National", "Bullingdon": "National", "Bure": "National",
+    "Cardiff": "National", "Channings Wood": "National", "Chelmsford": "National",
+    "Coldingley": "Outer London", "Cookham Wood": "National", "Dartmoor": "National",
+    "Deerbolt": "National", "Doncaster": "National", "Dovegate": "National",
+    "Downview": "Outer London", "Drake Hall": "National", "Durham": "National",
+    "East Sutton Park": "National", "Eastwood Park": "National", "Elmley": "National",
+    "Erlestoke": "National", "Exeter": "National", "Featherstone": "National",
+    "Feltham A": "Outer London", "Feltham B": "Outer London", "Five Wells": "National",
+    "Ford": "National", "Forest Bank": "National", "Fosse Way": "National",
+    "Foston Hall": "National", "Frankland": "National", "Full Sutton": "National",
+    "Garth": "National", "Gartree": "National", "Grendon": "National",
+    "Guys Marsh": "National", "Hatfield": "National", "Haverigg": "National",
+    "Hewell": "National", "High Down": "Outer London", "Highpoint": "National",
+    "Hindley": "National", "Hollesley Bay": "National", "Holme House": "National",
+    "Hull": "National", "Humber": "National", "Huntercombe": "National",
+    "Isis": "Inner London", "Isle of Wight": "National", "Kirkham": "National",
+    "Kirklevington Grange": "National", "Lancaster Farms": "National",
+    "Leeds": "National", "Leicester": "National", "Lewes": "National",
+    "Leyhill": "National", "Lincoln": "National", "Lindholme": "National",
+    "Littlehey": "National", "Liverpool": "National", "Long Lartin": "National",
+    "Low Newton": "National", "Lowdham Grange": "National", "Maidstone": "National",
+    "Manchester": "National", "Moorland": "National", "Morton Hall": "National",
+    "The Mount": "National", "New Hall": "National", "North Sea Camp": "National",
+    "Northumberland": "National", "Norwich": "National", "Nottingham": "National",
+    "Oakwood": "National", "Onley": "National", "Parc": "National", "Parc (YOI)": "National",
+    "Pentonville": "Inner London", "Peterborough Female": "National",
+    "Peterborough Male": "National", "Portland": "National", "Prescoed": "National",
+    "Preston": "National", "Ranby": "National", "Risley": "National", "Rochester": "National",
+    "Rye Hill": "National", "Send": "National", "Spring Hill": "National",
+    "Stafford": "National", "Standford Hill": "National", "Stocken": "National",
+    "Stoke Heath": "National", "Styal": "National", "Sudbury": "National",
+    "Swaleside": "National", "Swansea": "National", "Swinfen Hall": "National",
+    "Thameside": "Inner London", "Thorn Cross": "National", "Usk": "National",
+    "Verne": "National", "Wakefield": "National", "Wandsworth": "Inner London",
+    "Warren Hill": "National", "Wayland": "National", "Wealstun": "National",
+    "Werrington": "National", "Wetherby": "National", "Whatton": "National",
+    "Whitemoor": "National", "Winchester": "National", "Woodhill": "Inner London",
+    "Wormwood Scrubs": "Inner London", "Wymott": "National",
 }
 
 # ----------------------------
